c800f72c9e0562d909241d0e10eb3cbccdef0eb1
Pierre Hamon <pierrehamon@gmail.com>
2019-03-28 20:03:12 +0100

    Setup Clever Cloud config files

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_03_27_182211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "requests", force: :cascade do |t|
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
@@ -37,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_03_27_182211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "users"
 end
